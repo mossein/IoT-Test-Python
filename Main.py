d2fbc43be976a148c965f63f29cbf68e64c3c0f0
mohammad <mmoghaddasja@mun.ca>
2021-09-19 11:09:11 -0230

    key

--- Main.py
+++ Main.py
@@ -1,3 +1,14 @@
 from tuya_iot import TuyaOpenAPI
 from requests import get
 import random
+
+ipAddress = get('https://api.ipify.org').text
+
+accessId = ''
+accessKey = ''
+
+# For more info: https://developer.tuya.com/en/docs/iot/api-request?id=Ka4a8uuo1j4t4
+ENDPOINT = "https://openapi.tuyaus.com"
+
+userName = ''
+passWord = ''
