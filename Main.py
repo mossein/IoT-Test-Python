8736e80004e62bff25a6d63adf2d4d18e736b003
mohammad <mmoghaddasja@mun.ca>
2021-09-20 10:14:24 -0230

    comit

--- Main.py
+++ Main.py
@@ -7,7 +7,6 @@ ipAddress = get('https://api.ipify.org').text
 accessId = ''
 accessKey = ''
 
-# For more info: https://developer.tuya.com/en/docs/iot/api-request?id=Ka4a8uuo1j4t4
 ENDPOINT = "https://openapi.tuyaus.com"
 
 userName = ''
