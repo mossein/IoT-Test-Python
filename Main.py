d0a6b244ce39c93e79d0dec4d08f1e42eed5a83a
mohammad <mmoghaddasja@mun.ca>
2021-09-20 10:13:11 -0230

    added the function

--- Main.py
+++ Main.py
@@ -12,3 +12,26 @@ ENDPOINT = "https://openapi.tuyaus.com"
 
 userName = ''
 passWord = ''
+
+FINGERBOT_DEVICE_ID = ''
+LIGHTSTRIP_DEVICE_ID = ''
+
+DARK_BLUE = {'h': 246, 's': 100, 'v': 100}
+LIGHT_BLUE = {'h': 193, 's': 56, 'v': 100}
+WHITE = {'h': 1, 's': 1, 'v': 100}
+ORANGE = {'h': 23, 's': 90, 'v': 100}
+YELLOW = {'h': 62, 's': 81, 'v': 100}
+RED = {'h': 1, 's': 100, 'v': 100}
+
+openapi = TuyaOpenAPI(ENDPOINT, accessId, accessKey)
+openapi.login(userName, passWord)
+
+
+def get_weather():
+	location_url = f'/v1.0/iot-03/locations/ip?ip={PUBLIC_IP}'
+	location = openapi.get(location_url)['result']
+	lat, lon = location['latitude'], location['longitude']
+
+	weather_url = f"/v2.0/iot-03/weather/current?lat={lat}&lon={lon}"
+	weather = openapi.get(weather_url)['result']['current_weather']['temp']
+	return weather
